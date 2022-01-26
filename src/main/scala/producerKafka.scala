4d2dedc8b9487856f01e1d4c6906af034de1a294
Aissata SANO_2 <cheickfofanas@yahoo.fr>
2022-01-26 16:45:07 +0100

    Collecte + ingestion = les données sont dans les clusters kafka (Brokers)

---
+++ src/main/scala/producerKafka.scala
@@ -0,0 +1,44 @@
+
+import org.apache.kafka.clients.producer._
+import org.apache.kafka.clients.producer.ProducerConfig._
+import org.apache.kafka.common.serialization._
+import org.apache.log4j.{LogManager, Logger}
+import java.util._
+import org.apache.kafka.common.security.auth.SecurityProtocol
+
+/*
+  ************* 'Extends App' me permet de ne plus passer par une classe main. */
+
+object producerKafka /* extends App*/ {
+  private val trace_kafka = LogManager.getLogger("console")
+
+  def getProducerKafka (message : String): Unit ={
+    val props = new Properties()
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "")
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringDeserializer")
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put(ProducerConfig.ACKS_CONFIG, "all")
+    props.put("security.protocol", "SASL_PLAINTEXT")
+
+    val topic : String = ""
+
+    /*
+    ******************* Gestionnaire d''erreurs *******************
+     */
+    try
+      {
+        val twitterProducer = new KafkaProducer[String, String](props)
+        val twitterRecord = new ProducerRecord[String, String](topic, message)
+        twitterProducer.send(twitterRecord)
+      }
+    catch {
+      /*
+      ********************* Envoi du message d''erreur *****************
+      */
+      case ex : Exception => trace_kafka.error(s"erreur dans l'envoi du message. Détails de l'erreur : ${ex.printStackTrace()}")
+    }
+
+  }
+
+
+}
---
+++ src/main/scala/twitterClient.scala
@@ -0,0 +1,70 @@
+
+import com.twitter.hbc.httpclient.auth._
+import com.twitter.hbc.core.{Client, Constants}
+import com.twitter.hbc.core.endpoint.{StatusesFilterEndpoint, Location}
+import java.util.concurrent.{BlockingQueue, LinkedBlockingQueue, TimeUnit}
+import com.twitter.hbc.ClientBuilder
+import com.twitter.hbc.core.processor.StringDelimitedProcessor
+import producerKafka._
+
+import scala.collection.JavaConverters._
+import org.apache.log4j.{LogManager, Logger}
+
+class twitterClient {
+
+  /*
+  ************** 3 étapes : authenfication, récupération des messages spécifiques et construction d''un client hbc
+   */
+private  val trace_hbc = LogManager.getLogger("console")
+  /*
+  *************** 4 élements essentiels pour authentification Twitter sont les suivants en variables
+   */
+
+  /*
+  Création d'une méthode pour prendre les élements d'authentification et de location en compte
+   */
+
+  def getClient_Twitter(token: String, consumerSecret: String, consumerKey: String, tokenSecret: String, town_list: Location): Unit = {
+    val token = ""
+    val consumerSecret = ""
+    val consumerKey = ""
+    val tokenSecret = ""
+
+
+    val queue : BlockingQueue[String] = new LinkedBlockingQueue[String](1000000)
+    val auth : Authentication = new OAuth1(consumerKey, consumerSecret, token, tokenSecret)
+
+    val endp : StatusesFilterEndpoint = new StatusesFilterEndpoint()
+    endp.locations(List(town_list).asJava)
+    /*
+    *************** Récupération de la data *********************
+     */
+    val client_HBC = new ClientBuilder()
+      .hosts(Constants.STREAM_HOST)
+      .authentication(auth)
+      .gzipEnabled(true)
+      .endpoint(endp)
+      .processor(new StringDelimitedProcessor(queue)
+      )
+
+    val client_HBC_complete = client_HBC.build()
+
+    client_HBC_complete.connect()
+
+    try {
+      while (!client_HBC_complete.isDone) {
+        val tweet = queue.poll(300, TimeUnit.SECONDS)
+        getProducerKafka(tweet)
+      }
+
+    }catch {
+     case ex : Exception => trace_hbc.error(s"erreur dans le client HBC. Détails : ${ex.printStackTrace()}")
+    } finally {
+      client_HBC_complete.stop()
+    }
+
+  }
+
+}
+
+
